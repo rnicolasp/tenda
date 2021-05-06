bb3a2bd9ee870f955c78e3ff2240ceab20cb5aad
Ruben Orfila <rorfilar@esliceu.net>
2021-05-06 10:59:05 +0200

    0.0.12

--- insert_trabajadores.php
+++ insert_trabajadores.php
@@ -2,17 +2,15 @@
     <form action="insterttbr_api.php" method="post">
     <?php require 'includes/header.php';?>
     <label> Dni </label>
-        <input type="text" maxlenght="16" name="Dni" placeholder="Dni del trabajador"/>
+        <input type="text" maxlenght="2" name="Dni" required placeholder="Inserta tu DNI"/>
         <label> Nombre </label>
-        <input type="text" maxlenght="255" name="Nombre" placeholder="Nombre del trabajador"/>
+        <input type="text" maxlenght="255" name="Nombre" placeholder="Inserta tu nombre"/>
         <label> Apellidos </label>
-        <input type="text" maxlenght="255" name="Apellidos" placeholder="Apelllidos trabajador"/>
+        <input type="text" maxlenght="255" name="Apellidos" placeholder="Inserta tus apellidos"/>
         <label> Telefono </label>
-        <input type="tel" maxlenght="14" name="Telefono" placeholder="Teléfono trabajador"/>
+        <input type="tel" maxlenght="14" name="Telefono" placeholder="Inserta tu telefono"/>
         <label> Email </label>
-        <input type="text" maxlenght="100" name="Email" placeholder="Email del trabajador"/>
-        <label> Horario </label>
-        <input type="text" maxlenght="100" name="Horari" placeholder="Horario del trabajador"/>
+        <input type="text" maxlenght="100" name="Email" placeholder="Inserta tu email"/>
         <button type="submit"> Enviar</button>
     </form>
-</body>
+</body> 
