d9f051e4e3b4269a4e601e5081b405ab6768959a
Ruben Orfila <rorfilar@esliceu.net>
2021-06-11 13:22:51 +0200

    0.2.1

--- index.php
+++ index.php
@@ -6,22 +6,25 @@
         <div style="width: 100%; text-align: center;">
             <h1>Panaderia de PeepoDJ</h1>
             <?php require 'includes/header.php'; ?>
-            <h2> Vendemos pan fresco</h2>
+            <h2>Bienvenid@ a la panadería</h2>
         </div>
-        <div style="width: 100%; text-align: left;">  
-        <p> En la panaderia de PeepoDJ vendemos los mejores panes y la mejor reposteria de la ciudad.
-        Tenemos una amplia cantidad de productos de diferentes proveidores para siempre tener diversidad.</p>
+        <div style="width: 100%; text-align: center;"></div>
+        <p><i>Esta es una pequeña guía de como funciona la pagina web, una vez dicho esto, comencemos.</i></p>
         </div>
         <div style="width: 100%; text-align: center;">
             <img src="Images/pepoDJ.gif" width="200px" />
-        </div> 
-            <p> Todo los productos que cendemos en nuestra tienda son hechos a mano por pequeños 
-            proveidores que trabajan muy duro en ello para ofrecer un producto de altisima calidad. El mayor proveidor de panes actual es PeepoDJ. </p>
-            <div style="width: 100%; text-align: topright;">
-                <img src="Images/PAN.jpeg" width="250px" />
+        </div><div>
+    </div>
+        <div style="text-align: center;">
+            <p> Cuando clicas en el boton llamado "<b><u>Listas</u></b>" se abrirá el apartado de las listas disponibles en la pagina web.
+            </p><p>
+                Cuando clicas en el boton llamado "<b><u>Insertar</u></b>" se abrirá el apartado de las opciones posibles para insertar en la base de datos
+            </p>
         </div>
+            <div style="width: 100%; text-align: center;">
+                <img src="Images/PAN.jpeg" width="250px" />
             <p>"Lo de arriba es un pan bastante fresco"</p>
-            <p> -Sergio 2020/2021</p>
+            <p> R.I.P - Sergio 2020/2021</p>
         </div>
     </body>
 
